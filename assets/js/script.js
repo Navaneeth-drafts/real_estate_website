ac528c31896d28c196683ae3558ef804a1c599b8
Gantla Navaneeth Reddy <gantlanavaneethreddy@Navaneeths-M2.local>
2025-08-21 21:12:13 +0530

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+
+/**
+ * navbar toggle in mobile
+ */
+
+const /** {NodeElement} */ $navbar = document.querySelector("[data-navbar]");
+const $navToggler   = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => {
+    $navbar.classList.toggle("active");
+    // $navToggler.classList.toggle("active");
+});
+
+// header scroll state
+
+const /** {NodeElement} */ $header = document.querySelector("[data-header]");
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
